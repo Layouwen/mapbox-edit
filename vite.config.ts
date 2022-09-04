4e6e4a659800695a8d51fe438e9dbe2a2a8a843f
Avan <layouwen@gmail.com>
2022-09-04 18:58:36 +0800

    chore: 配置别名

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import * as path from "path";
+
+const getPath = (p) => path.resolve("./src", p);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      '/tile-map': {
-        target: 'https://tile.locnavi.com',
+      "/tile-map": {
+        target: "https://tile.locnavi.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/tile-map/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/tile-map/, ""),
+      },
+    },
+  },
+  resolve: {
+    alias: [
+      {
+        find: "@/components",
+        replacement: getPath("components"),
+      },
+    ],
+  },
+});
